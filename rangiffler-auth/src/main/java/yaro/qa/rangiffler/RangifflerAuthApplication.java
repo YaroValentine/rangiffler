d3a1744715bc59a01cd3ace0f455f32a3379d87c
yarov <yaroslav.vinokurov@gmail.com>
2023-07-01 16:47:46 -0400

    init setup

---
+++ rangiffler-auth/src/main/java/yaro/qa/rangiffler/RangifflerAuthApplication.java
@@ -0,0 +1,13 @@
+package yaro.qa.rangiffler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangifflerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RangifflerAuthApplication.class, args);
+    }
+}
+
