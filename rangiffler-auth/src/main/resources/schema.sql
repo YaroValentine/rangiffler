decadf9b0c8ccd42b0bceb059a68ed328d59570a
yarov <yaroslav.vinokurov@gmail.com>
2023-07-02 13:23:47 -0400

    updated auth schema.sql to create db if it doesn't exist

--- rangiffler-auth/src/main/resources/schema.sql
+++ rangiffler-auth/src/main/resources/schema.sql
@@ -1,23 +1,24 @@
-CREATE DATABASE IF NOT EXISTS `rangiffler-auth`;
-USE `rangiffler-auth`;
+ CREATE DATABASE IF NOT EXISTS `rangiffler-auth`;
+ USE `rangiffler-auth`;
 
-CREATE TABLE IF NOT EXISTS `users` (
-    `id`                        CHAR(36)        NOT NULL DEFAULT (UUID()),
-    `username`                  VARCHAR(50)     NOT NULL,
-    `password`                  VARCHAR(255)    NOT NULL,
-    `enabled`                   TINYINT(1)      NOT NULL,
-    `account_non_expired`       TINYINT(1)      NOT NULL,
-    `account_non_locked`        TINYINT(1)      NOT NULL,
-    `credentials_non_expired`   TINYINT(1)      NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`username`)
-    );
+CREATE TABLE IF NOT EXISTS users
+(
+    id                      BINARY(16)  unique not null default (UUID_TO_BIN(UUID(), TRUE)),
+    username                varchar(50) unique not null,
+    password                varchar(255)       not null,
+    enabled                 boolean            not null,
+    account_non_expired     boolean            not null,
+    account_non_locked      boolean            not null,
+    credentials_non_expired boolean            not null,
+    primary key (id, username)
+);
 
-CREATE TABLE IF NOT EXISTS `authorities` (
-    `id`                        CHAR(36) NOT NULL DEFAULT (UUID()),
-    `user_id`                   CHAR(36) NOT NULL,
-    `authority`                 VARCHAR(50) NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`)     REFERENCES `users` (`id`),
-    UNIQUE `ix_auth_username`   (`user_id`, `authority`)
-    );
+create table if not exists authorities
+(
+    id        BINARY(16) unique not null default (UUID_TO_BIN(UUID(), TRUE)) primary key,
+    user_id   BINARY(16)  not null,
+    authority varchar(50) not null,
+    foreign key (user_id) references users (id)
+);
+
+-- CREATE UNIQUE INDEX  ix_auth_username ON authorities (user_id, authority);
