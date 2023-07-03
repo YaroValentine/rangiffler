82ab8faa4e1fb89f833f38aeb05195eb3d95dff5
yarov <yaroslav.vinokurov@gmail.com>
2023-07-03 16:36:13 -0400

    getaway: - added profile local and security oauth2. - added implementations starters: web-services, spring-ws-core, oauth2-resource-server, actuator, starter-test; reactor-netty. - updated SecurityConfig.securityFilterChain for OAuth2 security.

--- rangiffler-gateway/src/main/java/org/rangiffler/config/SecurityConfig.java
+++ rangiffler-gateway/src/main/java/org/rangiffler/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package org.rangiffler.config;
 
 import org.rangiffler.cors.CorsCustomizer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,10 +12,12 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
+@Profile("local")
 public class SecurityConfig {
 
   private final CorsCustomizer corsCustomizer;
 
+  @Autowired
   public SecurityConfig(CorsCustomizer corsCustomizer) {
     this.corsCustomizer = corsCustomizer;
   }
@@ -22,10 +26,13 @@ public class SecurityConfig {
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     corsCustomizer.corsCustomizer(http);
 
-    http.authorizeHttpRequests(
-        authorize ->
-            authorize.anyRequest().permitAll()
-    ).csrf().disable();
+    http.authorizeHttpRequests()
+            .requestMatchers("/actuator/health").permitAll()
+            .anyRequest()
+            .authenticated().and()
+            .oauth2ResourceServer()
+            .jwt();
     return http.build();
   }
+
 }
